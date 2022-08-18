8417a134d10df63acd20c09d2421596b13b2bbea
danie <danie@DESKTOP-78HAKLI>
2022-08-18 19:07:08 +0200

    Add project files.

---
+++ Övningar Delegates and Events/Crocodile.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Övningar_Delegates_and_Events
+{
+    //1. ) 
+
+    //Skapa en klass som representerar ett valfritt djur. Det ska ha minst två events som motsvarar saker som djuret gör. Skriv metoder i klassen
+    //som anropar dina events vid rätt omständigheter.Testa genom att prenumerera på dina events med funktioner som skriver ut saker på konsolen.
+    internal class Crocodile
+    {
+        // Event 1
+        public event Action<string> Jump;
+        // Event 2
+        public event Action<string> Move;
+
+        //Funktioner som ska prenumerera på eventen
+        private void CrocodileJump(string text)
+        {
+            Console.WriteLine(text);
+        }
+
+        private void CrocodileMove(string text)
+        {
+            Console.WriteLine(text);
+        }
+
+        //Anropa Event 1 eller Event 2
+        public void DoJump()
+        {
+            Console.WriteLine("Jumping!");
+            Jump("Weird Crocodile who can jump");
+        }
+        public void DoMove()
+        {
+            Console.WriteLine("Moving!");
+            Move("Crocodile is Moving!");
+        }
+
+        // Konstruktor
+        public Crocodile()
+        {
+            // Prenumerera på Eventen Jump / Move med funktioner som skriver ut något(void) och tar exakt 1 Parameter av typen String.
+            Jump += CrocodileJump;
+            Move += CrocodileMove;
+        }
+    }
+    //2. )
+
+    //Skapa en klass som kan skriva ut saker på konsolen. Den ska ha ett event för händelsen att något skrivs ut. Skapa en ny klass,
+    //som prenumererar på utskriftshändelsen.
+    public class Writing
+    {
+        // Event för något som skrivs ut
+        public event Action WritingSomething;
+
+        public void Eating()
+        {
+            Console.WriteLine("Eating!");
+
+            // Triggar eventet som skapar en kedja av prenumererade funktionsanrop
+            WritingSomething();
+        }
+
+        public void Running()
+        {
+            Console.WriteLine("Running!");
+        }
+    }
+
+    public class Prenumerera
+    {
+        public void Subscribed()
+        {
+            Console.WriteLine("Subscribed on Event WritingSomething");
+        }
+    }
+
+}
---
+++ Övningar Delegates and Events/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace Övningar_Delegates_and_Events
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("In this program we are testing Delegates and Events!");
+            Console.ReadKey(true);
+            Crocodile myCrocodile = new Crocodile();
+            myCrocodile.DoJump();
+            myCrocodile.DoMove();
+            Writing myWriting = new Writing();
+            Prenumerera myPrenumerera = new Prenumerera();
+            // "Subscribar" med olika funktioner i olika klasser på Eventet WritingSomething!
+            myWriting.WritingSomething += myWriting.Running;
+            myWriting.WritingSomething += myPrenumerera.Subscribed;
+            // I denna funktion triggas en "kedja" av funktionsanrop genom eventet WritingSomething
+            myWriting.Eating();
+
+            Console.ReadKey(true);
+
+
+        }
+    }
+}
